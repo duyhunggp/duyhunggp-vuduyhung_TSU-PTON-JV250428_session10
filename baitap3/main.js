1c551b229ea9be1718f485e75bb9e80e6fac3bee
duyhunggp <vuduyhunggp.1998@gmail.com>
2025-06-06 01:40:17 +0900

    upload file

---
+++ baitap3/main.js
@@ -0,0 +1,5 @@
+let width = Number(prompt("nhap vao chieu rong: "));
+let height = Number(prompt("nhap vao chieu dai"));
+
+alert("dien tich la: " + width * height);
+console.log("dien tich la: " + width * height);
