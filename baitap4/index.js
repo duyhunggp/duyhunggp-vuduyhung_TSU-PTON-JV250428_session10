d0d2a959da4e8e21be2146a72526b0eb615b9a1a
duyhunggp <vuduyhunggp.1998@gmail.com>
2025-06-08 01:35:12 +0900

    upload file

---
+++ baitap4/index.js
@@ -0,0 +1,5 @@
+let firstNumber = Number(prompt("moi ban nhap so thu nhat: "));
+let secondNumber = Number(prompt("moi ban nhap so thu hai: "));
+let sum = firstNumber + secondNumber;
+alert("tong cua hai so ban vua nhap la: " + sum);
+console.log("tong cua hai so ban vua nhap la: " + sum);
